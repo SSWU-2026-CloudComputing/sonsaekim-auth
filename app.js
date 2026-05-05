43ac24df6d297d60a4a5792fdcb7e2ccc1e8373e
heeyeon919 <whgmldus1756@gmail.com>
2026-05-05 18:24:06 +0900

    feat: auth service basic setup and EJS rendering working

--- app.js
+++ app.js
@@ -9,13 +9,18 @@ const authRouter = require('./routes/auth');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.locals.user = req.session ? req.session.user : null;
+  next();
+});
+
 // view 설정 (EJS)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // 정적 파일
-app.use('/public', express.static(path.join(__dirname, 'public')));
-
+//app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 // 라우터 연결
 app.use('/auth', authRouter);
 
